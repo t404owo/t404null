bc83fa4ed831263829ad75b44b6b1313b76578e7
t404owo <tags4owo@gmail.com>
2025-12-07 20:14:41 +0100

    Update Supabase JS client version to 2

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -1,4 +1,4 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@latest";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2";
 
 const supabaseUrl = "https://owbamcqdmqetrgcznxva.supabase.co";
 const supabaseKey =
