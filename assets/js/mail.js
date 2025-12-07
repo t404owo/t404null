cc95f657ae59c74e99616ae9665dde47ca63929f
t404owo <tags4owo@gmail.com>
2025-12-07 20:31:09 +0100

    Update Supabase client import to latest version

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -1,4 +1,4 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@latest/+esm";
 
 const supabaseUrl = "https://owbamcqdmqetrgcznxva.supabase.co";
 const supabaseKey =
